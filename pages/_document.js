b03e110da257a5826bd73f52bd48121d830a33ff
PichuPerez <gabyperez.dev@gmail.com>
2020-05-02 17:44:59 -0500

    Fixed styled components SSR issue

--- pages/_document.js
+++ pages/_document.js
@@ -1,43 +1,30 @@
-import Document, { Head, Main, NextScript } from 'next/document'
-// Import styled components ServerStyleSheet
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
-    )
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement()
-
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags }
-  }
-
-  render() {
-    return (
-      <html>
-        <Head>
-          <title>Gaby Perez Dev | Fullstack Developer</title>
-          <link
-            rel="stylesheet"
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-            crossorigin="anonymous"
-          ></link>
-          {/* Step 5: Output the styles in the head  */}
-          {this.props.styleTags}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    )
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 }
