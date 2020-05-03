336bb8e17f1e3b0e19228fed198874e186779c38
PichuPerez <gabyperez.dev@gmail.com>
2020-05-02 20:55:52 -0500

    Added work history

--- components/WorkHistory.js
+++ components/WorkHistory.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Experience = styled.p`
-  padding-top: 80px;
+const Experience = styled.div`
+  padding-top: 30px;
   font-size: 80px;
   text-align: center;
 `
@@ -10,64 +10,186 @@ const PrixzLogo = styled.img`
   padding: 10px;
   object-fit: content;
 `
+const Timeline = styled.div`
+  padding-top: 40px;
+  height: 100vh;
+  position: relative;
+  max-width: 1200px;
+  margin: 0 auto;
+  &:after {
+    content: '';
+    position: absolute;
+    width: 6px;
+    background-color: purple;
+    top: 0;
+    bottom: 0;
+    left: 50%;
+    margin-left: -3px;
+  }
+  @media screen and (max-width: 600px) {
+    &:after {
+      left: 31px;
+    }
+  }
+`
 
+/* Container around content */
+const Container = styled.div`
+  padding: 10px 40px;
+  position: relative;
+  background-color: inherit;
+  width: 50%;
+  &:after {
+    content: '';
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    right: -11px;
+    background-color: white;
+    border: 4px solid purple;
+    top: 15px;
+    border-radius: 50%;
+    z-index: 1;
+  }
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    padding-left: 70px;
+    padding-right: 25px;
+    &:before {
+      left: 60px;
+      border: medium solid white;
+      border-width: 10px 10px 10px 0;
+      border-color: transparent white transparent transparent;
+    }
+  }
+`
+const LeftContainer = styled(Container)`
+  left: -75px;
+  &:before {
+    content: ' ';
+    height: 0;
+    position: absolute;
+    top: 22px;
+    width: 0;
+    z-index: 1;
+    right: 30px;
+    border: medium solid white;
+    border-width: 10px 0 10px 10px;
+    border-color: transparent transparent transparent #a299a840;
+    @media screen and (max-width: 600px) {
+      &:after {
+        left: 15px;
+      }
+    }
+  }
+`
+const RightContainer = styled(Container)`
+  left: 50%;
+  &:before {
+    content: ' ';
+    height: 0;
+    position: absolute;
+    top: 22px;
+    width: 0;
+    z-index: 1;
+    left: 30px;
+    border: medium solid white;
+    border-width: 10px 10px 10px 0;
+    border-color: transparent #a299a840 transparent transparent;
+  }
+  &:after {
+    left: -16px;
+  }
+  @media screen and (max-width: 600px) {
+    left: 0%;
+    &:after {
+      left: 15px;
+    }
+  }
+`
+
+const Content = styled.div`
+  padding: 20px 30px;
+  background-color: #a299a840;
+  position: relative;
+  border-radius: 6px;
+`
+const LogoPrixz = styled.img`
+  height: 30px;
+`
+const LogoStylindex = styled.img`
+  height: 30px;
+`
+const LogoPrepared = styled.img`
+  height: 50px;
+`
+const ContainerLogoPrepared = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 40px;
+  color: black;
+`
+const PreparedText = styled.p`
+  color: #475568;
+  font-size: 25px;
+  margin-left: 5px;
+  margin-top: 10px;
+  font-family: Open Sans, sans-serif;
+`
 const WorkHistory = () => {
   return (
-    <div>
-      <div className="row justify-content-center">
-        <Experience>Experience</Experience>
-      </div>
-      <div className="row justify-content-around container">
-        <div className="col-md-12 col-lg-6">
-          <div class="card" style={{ width: '20rem' }}>
-            <PrixzLogo
-              src="http://old.prixz.com/public/img/brand-black@3x.png"
-              class="card-img-top"
-              alt="..."
-            ></PrixzLogo>
-            <div class="card-body">
-              <h5 class="card-title">Fullstack Developer @ Prixz</h5>
-              <p class="card-text">
-                Worked with technologies like Vue, node, hapi to support a
-                e-commerce for pharmaceutical products. Created an API for a
-                delivery service. Stabilized the invoice module front to back.
-              </p>
-              <a
-                href="http://old.prixz.com/"
-                class="btn btn-light"
-                target="_blank"
-              >
-                See project
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-12 col-lg-6">
-          <div class="card" style={{ width: '20rem' }}>
-            <PrixzLogo
-              src="http://old.prixz.com/public/img/brand-black@3x.png"
-              class="card-img-top"
-              alt="..."
-            ></PrixzLogo>
-            <div class="card-body">
-              <h5 class="card-title">Fullstack Developer @ Prixz</h5>
-              <p class="card-text">
-                Worked with technologies like Vue, node, hapi to support a
-                e-commerce for pharmaceutical products. Created an API for a
-                delivery service. Stabilized the invoice module front to back.
-              </p>
-              <a
-                href="http://old.prixz.com/"
-                class="btn btn-light"
-                target="_blank"
-              >
-                See project
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Experience>Work history</Experience>
+      <Timeline>
+        <LeftContainer>
+          <Content>
+            <h2>Front End Developer @ </h2>
+            <ContainerLogoPrepared>
+              <LogoPrepared src="/preparedLogo.png" />
+              <PreparedText> THE PREPARED</PreparedText>
+            </ContainerLogoPrepared>
+            <p>
+              Working with React embedded in a Wordpress site. Created a CMS in
+              React for kits data analysis. Created the kit builder app in React
+              following the UI/UX style guidelines.
+            </p>
+            <a href="https://theprepared.com/" target="_blank">
+              See project
+            </a>
+          </Content>
+        </LeftContainer>
+        <RightContainer>
+          <Content>
+            <h2>Front End Developer @ </h2>
+            <LogoStylindex src="/stylindexLogo.svg" />
+            <p>
+              Worked with React and Apollo to create new features within the
+              site and CMS. Created toast notifications. Added a scrollable
+              feature to media content. Improved visual design for artis notes
+              module.
+            </p>
+            <a href="https://stylindex.com/" target="_blank">
+              See project
+            </a>
+          </Content>
+        </RightContainer>
+        <LeftContainer>
+          <Content>
+            <h2>Fullstack Developer @ </h2>
+            <LogoPrixz src="/logoPrixz.png" />
+            <p>
+              Worked with technologies like Vue, node, hapi, React and Apollo to
+              support an e-commerce for pharmaceutical products. Created an API
+              for a delivery service between branch offices. Debugged and
+              stabilizeed the invoice module front to back.
+            </p>
+            <a href="http://old.prixz.com/" target="_blank">
+              See project
+            </a>
+          </Content>
+        </LeftContainer>
+      </Timeline>
+    </>
   )
 }
 
